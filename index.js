c996cf5bacd0d0fd2f795b2c9ede2d7d8b264db9
felix mwendwa <felix.mwendwa@student.moringaschool.com>
2022-08-22 00:26:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 11;
+let num4 = 7;
 
-// Write your code here
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = num3 % num4;
+const max = Math.max(12, 14, 16, 18, 20);
